c69ddb5867656b467d12b8fee0df470c190513d9
Kamran Ali <kamranalinitb@gmail.com>
2019-05-27 05:22:22 +0530

    Topic: Lazy Evaluation

---
+++ src/in/kamranali/fp/LazyEvaluation.scala
@@ -0,0 +1,106 @@
+package in.kamranali.fp
+
+object LazyEvaluation extends App {
+
+  // val x1: Int = throw new RuntimeException() // Crash the program
+
+  lazy val x2: Int = throw new RuntimeException() // DOESN'T  Crash the program
+
+  /*
+  Lazy Values are evaluated when they are used for the first time
+   */
+
+  //println(x2) // Crash the program
+
+  lazy val x3: Int = {
+    println("Hello")
+    42
+  }
+
+  println(x3) // x3 is evaluated. Prints `Hello`.
+  println(x3) // x3 is NOT Re-evaluated again
+
+  // Examples of implications: With Side Effects
+  def sideEffectCondition: Boolean = {
+    println("Boo")
+    true
+  }
+
+  def simpleCondition: Boolean = false
+
+  lazy val lazyCondition = sideEffectCondition
+
+  println(if (simpleCondition && lazyCondition) "yes" else "no" )
+
+  /*
+
+   */
+  // Examples of implications: In Conjunction with call by name
+  def byNameMethod1(n: => Int): Int = n + n + n + 1
+
+  def retrieveMagicValue = {
+    // side effect or a long computation
+    Thread.sleep(1000)
+    println("Waiting")
+    42
+  }
+
+  println(byNameMethod1(retrieveMagicValue)) // 3 sec Waiting time
+
+
+  // use lazy vals
+  def byNameMethod2(n: => Int): Int = {
+    lazy val t = n // Only evaluated once i.e. CALL BY NEED. Then use it when needed
+    t + t + t + 1
+  }
+
+  println(byNameMethod2(retrieveMagicValue)) // 1 sec waiting
+
+  /*
+  Filtering with Lazy vals
+   */
+  def lessThan30(i: Int): Boolean = {
+    println(s"$i is less than 30?")
+    i < 30
+  }
+
+  def greaterThan20(i: Int): Boolean = {
+    println(s"$i is greater than 20?")
+    i > 20
+  }
+
+  val numbers = List(1, 25, 40, 5, 23)
+  val lt30 = numbers.filter(lessThan30) // List(1,25,5,23)
+  val gt20 = lt30.filter(greaterThan20)
+
+  println(gt20)
+
+  val lt30lazy = numbers.withFilter(lessThan30)
+  val gt20Lazy = lt30lazy.withFilter(greaterThan20)
+
+  // `withFilter` uses lazy values under the hood
+  println()
+  println(gt20Lazy) // <- `lessThan30` and `greaterThan20` methods are never called in lazy version
+
+  gt20Lazy.foreach(println) // <- this will force the filtering to take place
+  // If we look at result we can see, Predicates are being check on bi need basis
+
+  /*
+  For-comprehension use withFilter with guards
+   */
+
+  for {
+    a <- List(1,2,3) if a % 2 == 0 // use lazy vals
+  } yield a + 1
+
+  // Above For-comprehension translates to
+  List(1,2,3).withFilter(_ % 2 == 0).map(_ + 1) // List[Int]
+
+
+
+
+
+
+
+
+}
---
+++ src/in/kamranali/fp/MyStream.scala
@@ -0,0 +1,124 @@
+package in.kamranali.fp
+
+import scala.annotation.tailrec
+
+abstract class MyStream[+A] {
+
+  def isEmpty: Boolean
+  def head: A
+  def tail: MyStream[A]
+
+  def #::[B >: A](element: B): MyStream[B] // prepend operator
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] // concatenate two streams
+
+  def foreach(f: A => Unit): Unit
+  def map[B](f: A => B): MyStream[B]
+  def flatMap[B](f: A => MyStream[B]): MyStream[B]
+  def filter(predicate: A => Boolean): MyStream[A]
+
+  def take(n: Int): MyStream[A] // takes the first n elements out of this stream and returns it
+  def takeAsList(n: Int): List[A] = take(n).toList()
+
+  /*
+  [1 2 3 ].toList([]) =
+    [2 3].toList([1]) =
+      [3].toList([2 1]) =
+        [].toList([3 2 1]) =
+          [1 2 3]
+   */
+  @tailrec
+  final def toList[B >: A](acc: List[B] = Nil): List[B] =
+    if (isEmpty) acc.reverse
+    else tail.toList(head  :: acc)
+}
+
+object EmptyStream extends MyStream[Nothing] {
+  override def isEmpty: Boolean = true
+
+  override def head: Nothing = throw new NoSuchElementException
+
+  override def tail: MyStream[Nothing] = throw new NoSuchElementException
+
+  override def #::[B >: Nothing](element: B): MyStream[B] = new Cons[B](element, this)
+
+  override def ++[B >: Nothing](anotherStream: => MyStream[B]): MyStream[B] = anotherStream
+
+  override def foreach(f: Nothing => Unit): Unit = ()
+
+  override def map[B](f: Nothing => B): MyStream[B] = this
+
+  override def flatMap[B](f: Nothing => MyStream[B]): MyStream[B] = this
+
+  override def filter(predicate: Nothing => Boolean): MyStream[Nothing] = this
+
+  override def take(n: Int): MyStream[Nothing] = this
+}
+
+class Cons[+A](hd: A, tl: => MyStream[A]) extends MyStream[A] { // without call by Name (: =>) stream will not be lazily evaluated
+  override def isEmpty: Boolean = false
+
+  override val head: A = hd
+
+  override lazy val tail: MyStream[A] = tl  // `: =>` + `lazy` == CALL BY NEED
+
+  override def #::[B >: A](element: B): MyStream[B] = new Cons(element, this)
+
+  override def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
+
+  override def foreach(f: A => Unit): Unit = {
+    f(head)
+    tail.foreach(f)
+  }
+
+  override def map[B](f: A => B): MyStream[B] = new Cons(f(head), tail.map(f))  // preserves lazy evaluation
+
+  override def flatMap[B](f: A => MyStream[B]): MyStream[B] = f(head) ++ tail.flatMap(f)
+
+  override def filter(predicate: A => Boolean): MyStream[A] = { // preserves lazy evaluation
+    if (predicate(head)) new Cons(head, tail.filter(predicate))
+    else tail.filter(predicate)
+  }
+
+  override def take(n: Int): MyStream[A] =
+    if (n <= 0) EmptyStream
+    else if (n == 1) new Cons(head, EmptyStream)
+    else new Cons(head, tail.take(n - 1)) // preserves lazy evaluation
+}
+
+object MyStream {
+  def from[A](start: A)(generator: A => A): MyStream[A] =
+    new Cons(start, MyStream.from(generator(start))(generator))
+}
+
+object StreamPlayground extends App {
+  val naturals = MyStream.from(1)(_ + 1)
+
+  println(naturals.head)
+  println(naturals.tail.head)
+  println(naturals.tail.tail.head)
+
+  val startFrom0 = 0 #:: naturals
+  println(startFrom0.head)
+
+  startFrom0.take(10000).foreach(println)
+
+  println(startFrom0.map(_ * 2).take(100).toList())
+
+  println(startFrom0.flatMap(x => new Cons(x, new Cons(x + 1, EmptyStream))).take(10).toList())
+
+  println(startFrom0.filter(_ < 10).take(10).toList())
+
+  // Streams of Fibonacci numbers
+  def fibonacci(first: BigInt, second: BigInt): MyStream[BigInt] =
+    new Cons[BigInt](first, fibonacci(second, first + second))
+
+  println(fibonacci(1,1).take(10).toList())
+
+  // Stream of prime numbers with Eratosthenes' sieve
+  def eratosthenes(numbers: MyStream[Int]): MyStream[Int] =
+    if (numbers.isEmpty) numbers
+    else new Cons[Int](numbers.head, eratosthenes(numbers.tail.filter(_ % numbers.head != 0)))
+
+  println(eratosthenes(MyStream.from(2)(_ + 1)).take(20).toList())
+
+}
