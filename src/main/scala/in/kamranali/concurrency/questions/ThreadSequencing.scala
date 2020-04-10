9e4f2965fc8f6240efdb940da0726f57b824b85c
Kamran Ali <kamranalinitb@gmail.com>
2020-04-10 11:58:06 +0530

    Removed lock object and synchronizing on List buffer itself

--- src/main/scala/in/kamranali/concurrency/questions/ThreadSequencing.scala
+++ src/main/scala/in/kamranali/concurrency/questions/ThreadSequencing.scala
@@ -2,13 +2,15 @@ package in.kamranali.concurrency.questions
 
 import java.util.concurrent.Executors
 
+import scala.collection.mutable
+
 /**
   *
   * Reference: http://www.devinline.com/2016/01/print-pattern-1-2-3-using-three-threads.html
   */
-case class Sequence(sequence: Int) extends Runnable {
+case class Consumer(sequence: Int) extends Runnable {
 
-  import Sequence._
+  import Consumer._
 
   override def run(): Unit = {
 
@@ -17,17 +19,17 @@ case class Sequence(sequence: Int) extends Runnable {
       // Adding sleep to slow down the processing
       Thread.sleep(500)
 
-      monitor.synchronized {
+      data.synchronized {
 
         // First Thread handled here
         if (sequence == 1) {
           if (!one) {
-            monitor.wait()
+            data.wait()
           } else {
 
-            if (data.nonEmpty) {
-              println(s"Thread $sequence Consumed ${data.head}")
-              data = data.tail
+            if (iterator != capacity) {
+              println(s"Thread $sequence Consumed ${data(iterator)}")
+              iterator += 1
             } else {
               println(s"Thread $sequence has not consumed anything. Data is empty")
             }
@@ -36,19 +38,19 @@ case class Sequence(sequence: Int) extends Runnable {
             one = false
             two = true // We are signalling second thread to proceed
             three = false
-            monitor.notifyAll()
+            data.notifyAll()
           }
         }
 
         // Second Thread handled here
         if (sequence == 2) {
           if (!two) {
-            monitor.wait()
+            data.wait()
           } else {
 
-            if (data.nonEmpty) {
-              println(s"Thread $sequence Consumed ${data.head}")
-              data = data.tail
+            if (iterator != capacity) {
+              println(s"Thread $sequence Consumed ${data(iterator)}")
+              iterator += 1
             } else {
               println(s"Thread $sequence has not consumed anything. Data is empty")
             }
@@ -56,19 +58,19 @@ case class Sequence(sequence: Int) extends Runnable {
             one = false
             two = false
             three = true // We are signalling third thread to proceed
-            monitor.notifyAll()
+            data.notifyAll()
           }
         }
 
         // Third Thread handled here
         if (sequence == 3) {
           if (!three) {
-            monitor.wait()
+            data.wait()
           } else {
 
-            if (data.nonEmpty) {
-              println(s"Thread $sequence Consumed ${data.head}")
-              data = data.tail
+            if (iterator != capacity) {
+              println(s"Thread $sequence Consumed ${data(iterator)}")
+              iterator += 1
             } else {
               println(s"Thread $sequence has not consumed anything. Data is empty")
             }
@@ -76,7 +78,7 @@ case class Sequence(sequence: Int) extends Runnable {
             one = true // We are signalling first thread to proceed
             two = false
             three = false
-            monitor.notifyAll()
+            data.notifyAll()
           }
         }
 
@@ -85,28 +87,27 @@ case class Sequence(sequence: Int) extends Runnable {
   }
 }
 
-object Sequence {
-
-  val monitor = new Object()
+object Consumer {
 
   // This will facilitate synchronization between threads
   var one: Boolean = true // Initially we will let first thread to proceed
   var two: Boolean = false
   var three: Boolean = false
 
-  // This is the data that we are consuming via multiple threads
-  var data = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  // This is the data that we are consuming via multiple threads. We will take lock on this object
+  val data: mutable.ListBuffer[Int] = mutable.ListBuffer(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  var iterator = 0
+  private val capacity = data.length //<- Exit Condition
 
 }
+
 object ThreadSequencing {
 
   def main(args: Array[String]): Unit = {
 
-    // Reference -> http://www.devinline.com/2016/01/print-pattern-1-2-3-using-three-threads.html
-
-    val consumer1 = new Thread(Sequence(1))
-    val consumer2 = new Thread(Sequence(2))
-    val consumer3 = new Thread(Sequence(3))
+    val consumer1 = new Thread(Consumer(1))
+    val consumer2 = new Thread(Consumer(2))
+    val consumer3 = new Thread(Consumer(3))
 
     val pool = Executors.newFixedThreadPool(3)
     pool.execute(consumer1)
