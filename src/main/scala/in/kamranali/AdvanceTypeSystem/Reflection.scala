832da49a3404f9d707b76052aa4bec4c1ab9944a
Kamran Ali <kamranalinitb@gmail.com>
2020-10-10 11:37:38 +0530

    Changes during revision

--- src/main/scala/in/kamranali/AdvanceTypeSystem/Reflection.scala
+++ src/main/scala/in/kamranali/AdvanceTypeSystem/Reflection.scala
@@ -1,7 +1,7 @@
 package in.kamranali.AdvanceTypeSystem
 
 /**
-  * Advance Scala Lesson 52 [Reflection]
+  * Advance Scala Lesson 52 [Reflection 1]
   *
   * Ref
   * - https://www.udemy.com/course/advanced-scala/learn/lecture/11053880
@@ -10,8 +10,10 @@ package in.kamranali.AdvanceTypeSystem
 
 object Reflection extends App {
 
-  // How to instantiate a class Or invoke a method by calling its name DYNAMICALLY at Runtime ??
-  // This is problem of Reflection, which is the ability of JVM to operate on classes and instances and call methods at Runtime.
+  /*
+     How to instantiate a class Or invoke a method by calling its name DYNAMICALLY at Runtime ??
+     This is problem of Reflection, which is the ability of JVM to operate on classes and instances and call methods at Runtime.
+   */
 
   case class Person(name: String) {
     def sayMyName(): Unit = println(s"Hi, my name is $name")
@@ -20,48 +22,48 @@ object Reflection extends App {
   // Step = 0 (Do the import)
   import scala.reflect.runtime.{universe => ru}
 
-  // Step = 1 instantiate Mirror (which is something which reflect things)
-  val m = ru.runtimeMirror(getClass.getClassLoader) //<- `getClass.getClassLoader` returns Class Loader for `Reflection`
+  // Step = 1 Instantiate Mirror (which is something that reflect things)
+  val m: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader) //<- `getClass.getClassLoader` returns Class Loader for `Reflection`
 
   // Step = 2 Create class object by Name
-  val clazz = m.staticClass("in.kamranali.AdvanceTypeSystem.Reflection.Person")
+  val clazz:ru.ClassSymbol = m.staticClass("in.kamranali.AdvanceTypeSystem.Reflection.Person")
   // `clazz` is a `ClassSymbol` i.e. its kind of like a description of class
 
   // Step 3 create a reflected mirror
-  val cm = m.reflectClass(clazz)
-  // `cm` is a `ClassMirror` which can access the members of the `ClassSymbol` e.g. constructors and can do things
+  val cm: ru.ClassMirror = m.reflectClass(clazz)
+  // `cm` is a `ClassMirror` which can access the members of the `ClassSymbol` e.g. constructors, methods and can do things
 
   // Step 4 get the constructor
-  val constructor = clazz.primaryConstructor.asMethod
-  // `constructor` is a `MethodSymbol` describing method params etc
+  val constructor: ru.MethodSymbol = clazz.primaryConstructor.asMethod
+  // `constructor` is a `MethodSymbol` which describes the method i.e. What kind of parameter is gets, What kind of return types it has and so on and so forth.
 
   //Step 5 Reflect the constructor
-  val constructorMirror = cm.reflectConstructor(constructor)
+  val constructorMirror: ru.MethodMirror = cm.reflectConstructor(constructor)
   // `constructorMirror` is a Method mirror
 
   // Step 6 Invoke the constructor
-  val instance = constructorMirror.apply("John")
+  val instance: Any = constructorMirror.apply("John")
 
   // Basically we instantiated a dynamically computed class Name at Runtime with some arguments
-  println(instance)
+  println(instance) // Person(John)
 
   // USE CASE 2
   // Suppose I have an instance which I have received over the wire as serialized object.
-  val p = Person("Mary") //<- We don't know the type of `p`
+  val p: Any = Person("Mary") //<- We don't know the type of `p`
 
   // Method name is computed somewhere else
   val methodName = "sayMyName"
 
-  // 1. mirror
+  // 1. Obtain the Mirror
   // 2. reflect the instance
-  val reflected = m.reflect(p)
+  val reflected: ru.InstanceMirror = m.reflect(p)
 
   // 3. method symbol
-  val methodSymbol = ru.typeOf[Person].decl(ru.TermName(methodName)).asMethod
+  val methodSymbol: ru.MethodSymbol = ru.typeOf[Person].decl(ru.TermName(methodName)).asMethod
 
   // 4 Reflect the method
-  val method = reflected.reflectMethod(methodSymbol)
+  val method: ru.MethodMirror = reflected.reflectMethod(methodSymbol)
 
   // 5 invoke
-  method.apply()
+  method.apply() // Hi, my name is Mary
 }
--- src/main/scala/in/kamranali/AdvanceTypeSystem/TypeErasure.scala
+++ src/main/scala/in/kamranali/AdvanceTypeSystem/TypeErasure.scala
@@ -1,6 +1,12 @@
 package in.kamranali.AdvanceTypeSystem
 
-import scala.reflect.runtime.universe
+/**
+  * Advance Scala Lesson 53 [Reflection 2: Type Erasure]
+  *
+  * Ref
+  * - https://www.udemy.com/course/advanced-scala/learn/lecture/11053880
+  *
+  */
 
 object TypeErasure extends App {
 
@@ -16,11 +22,14 @@ object TypeErasure extends App {
   }  //<- will print `List of string` even though numbers is List of Integers because of Type Erasures
 
   // 2. limitations on overloads
-//  def processList(list: List[Int]): Int = 43
-//  def processList(list: List[String]): Int = 43
-    // Compilation Error because both methods have identical definition post compilation
-
   /*
+        def processList(list: List[Int]): Int = 43
+        def processList(list: List[String]): Int = 43
+
+        Compilation Error because both methods have identical definition post compilation
+   */
+
+  /**
     TYPE TAGS:
       Scala's reflection API work around which allows carrying the complete Generic Type at Compile time to the Runtime
    */
@@ -31,25 +40,30 @@ object TypeErasure extends App {
 
   // 0 - Imports
   import scala.reflect.runtime.universe._
-  val ttag = typeTag[Person] //<- Returns `typeTag` instance of type `Person`
 
   // 1 - creating a Type Tag "manually" (Preferred way is to pass TypeTag as implicit parameters)
-  println(ttag.tpe)
+  val ttag: TypeTag[Person] = typeTag[Person] //<- Returns `typeTag` instance of type `Person`
+  val t: Type = ttag.tpe // `t` contains all the type information contained within that `t` type.
+  println(t) // in.kamranali.AdvanceTypeSystem.TypeErasure.Person
+  // ^ Creating a `typeTag` is done by the compiler and that information contained within that type tag is carried to the runtime
+  // By calling the `tpe` method, I have access to a type instance.
+  // Some type tags are basically used as a type evidence which then allows us to inspect generic types
+
 
   class MyMap[K, V]
 
   // 2 Pass TypeTags as implicit parameters and compiler will take care of creating them for us.
-  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]) = typeTag.tpe  match { //<- `typeTag` is created by the compiler when it does implicit inference
+  def getTypeArguments[T](value: T)(implicit typeTag: TypeTag[T]): List[Type] = typeTag.tpe  match { //<- `typeTag` is created by the compiler when it does implicit inference
     case TypeRef(_, _, typeArguments) => typeArguments
     case _ => List()
   } //<- Basically it returns the List of Generic parameters that `T` type was decorated with
 
   val myMap = new MyMap[Int, String]
   val typeArgs = getTypeArguments(myMap)
-  // Compiler will create a Type Tag of `typeTag: TypeTag[MyMap[Int, String]]` for us. So the types can safely be erased after compilation
+  // Compiler will create an implicit TypeTag of `typeTag: TypeTag[MyMap[Int, String]]` for us. So the types can safely be erased after compilation
     // At runtime I get access of `tpe` member of `TypeTag` that compiler created originally
 
-  println(typeArgs)
+  println(typeArgs) // List(Int, String)
 
   // Since we now have Type information at runtime we can do a bunch of operations
 
@@ -66,15 +80,15 @@ object TypeErasure extends App {
     // We can create Method Symbol from TypeTag
 
   import scala.reflect.runtime.{universe => ru}
-  val p = Person("Mary") //<- We don't know the type of `p`
+  val p: Any = Person("Mary") //<- We don't know the type of `p`
   val methodName = "sayMyName" //<- Method name is computed somewhere else
 
   // 3. method symbol is calculated from Type Tag
   val methodSymbol = typeTag[Person].tpe.decl(ru.TermName(methodName)).asMethod
 
-  val m = ru.runtimeMirror(getClass.getClassLoader)
-  val reflected = m.reflect(p)
-  val method = reflected.reflectMethod(methodSymbol)
+  val m: Mirror = ru.runtimeMirror(getClass.getClassLoader)
+  val reflected: InstanceMirror = m.reflect(p)
+  val method: MethodMirror = reflected.reflectMethod(methodSymbol)
 
   // 5 invoke
   method.apply()
