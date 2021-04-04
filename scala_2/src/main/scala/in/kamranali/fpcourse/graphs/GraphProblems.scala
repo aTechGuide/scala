cef2474e069f34dd5c6d68e2ca994214502fe071
Kamran Ali <kamranalinitb@gmail.com>
2021-04-04 13:55:33 +0530

    Added isPath in GraphProblems

--- scala_2/src/main/scala/in/kamranali/fpcourse/graphs/GraphProblems.scala
+++ scala_2/src/main/scala/in/kamranali/fpcourse/graphs/GraphProblems.scala
@@ -1,5 +1,7 @@
 package in.kamranali.fpcourse.graphs
 
+import scala.annotation.tailrec
+
 object GraphProblems extends App {
 
   // Every Node will be a Key in the Map
@@ -24,8 +26,46 @@ object GraphProblems extends App {
   // number of nodes connected to `node`
   def inDegree[T](graph: Graph[T], node: T): Int = graph.values.count(_.contains(node))
 
+  /**
+    * Medium Problems
+    */
+
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+
+    /*
+      Alice -> Mary
+      ipt([Alice], []) =
+      ipt([Bob, Charlie, David], [Alice]) =
+      ipt([Charlie, David], [Bob, Alice]) =
+      ipt([David, David], [Charlie, Bob, Alice]) =
+      ipt([David, Bob, Mary], [David, Charlie, Bob, Alice]) =
+      ipt([Bob, Mary], [David, Charlie, Bob, Alice]) =
+      ipt([Mary], [David, Charlie, Bob, Alice]) =
+      = true
+      N nodes, E edges
+      Complexity: O(E)
+     */
+
+    @tailrec
+    def isPathTailrec(remaining: List[T], consideredNodes: Set[T]): Boolean = {
+      if (remaining.isEmpty) false
+      else {
+        val node = remaining.head
+
+        if (node == end) true
+        else if (consideredNodes.contains(node)) isPathTailrec(remaining.tail, consideredNodes)
+        else isPathTailrec(remaining.tail ++ graph(node), consideredNodes + node)
+      }
+    }
+
+    isPathTailrec(List(start), Set())
+  }
+
   assert(outDegree(socialNetwork, "Alice") == 3)
   assert(inDegree(socialNetwork, "David") == 2)
+
+  assert(isPath(socialNetwork, "Alice", "Mary"))
+  assert(!isPath(socialNetwork, "Bob", "Mary"))
 
 
 }
